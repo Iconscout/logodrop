05d0af7069454e50cb229f44b953feb23033d4ad
Tarun Mangukiya <tarunmangukiya@users.noreply.github.com>
2017-09-09 13:35:25 +0530

    Platfor Compilation

--- src/common/config.js
+++ src/common/config.js
@@ -1,14 +1,14 @@
-var Iconscout = Iconscout || {};
-Iconscout.config = {
+var Logodrop = Logodrop || {};
+Logodrop.config = {
   API_URL: 'https://api.iconscout.com',
   ACCESS_TOKEN: 'rzSWZK6TQEwrj4jvVuuxP7CJu6qaSrPS'
 };
 
-Iconscout.enums = {
+Logodrop.enums = {
   EXPORT_MODE: {
     ALL: 'all',
     SELECTION: 'selection',
   },
 };
 
-export default Iconscout
+export default Logodrop
